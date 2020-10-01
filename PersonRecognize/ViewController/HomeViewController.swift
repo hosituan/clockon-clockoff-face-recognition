46e72306610cce0c77f52f960d5a8e403f9905b3
hosituanit <hosituan.work@gmail.com>
2020-10-01 10:44:12 +0700

    Save data to Realm, use for local data.
    Former-commit-id: 3ecc80c85a15b0ffcbc78fb4232314484c6e4c88

--- PersonRecognize/ViewController/HomeViewController.swift
+++ PersonRecognize/ViewController/HomeViewController.swift
@@ -20,42 +20,15 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var vectorsLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadData()
+        if !NetworkChecker.isConnectedToInternet {
+            showDialog(message: "You have not connected to internet. Using local data.")
+        }
+
     }
     override func viewWillAppear(_ animated: Bool) {
         navigationController?.isNavigationBarHidden = true
-        if NetworkChecker.isConnectedToInternet {
-            ProgressHUD.show("Loading users...")
-            fb.loadVector { [self] (result) in
-                kMeanVectors = result
-                print("Numver of k-Means vectors: \(kMeanVectors.count)")
-                vectorsLabel.text = "You have \(kMeanVectors.count / NUMBER_OF_K) users."
-                ProgressHUD.dismiss()
-                for vector in kMeanVectors {
-                    vectorHelper.saveVector(vector)
-                }
-            }
-            fb.loadLogTimes { (result) in
-                attendList = result
-                for user in attendList {
-                    let u = User(name: user.name, image: UIImage(named: "LaunchImage")!, time: user.time)
-                    localUserList.append(u)
-                }
-            }
-            
-            
-        }
-        else {
-//            code for local data
-            let result = realm.objects(SavedVector.self)
-            kMeanVectors = []
-            for vector in result {
-                let v = Vector(name: vector.name, vector: stringToArray(string: vector.vector), distance: vector.distance)
-                kMeanVectors.append(v)
-            }
-
-            vectorsLabel.text = "You have \(kMeanVectors.count / NUMBER_OF_K) users."
-            showDialog(message: "You have not connected to internet. Using local data.")
-        }
+        loadData()
     }
     override func viewDidAppear(_ animated: Bool) {
         fnet.clean()
@@ -104,6 +77,42 @@ class HomeViewController: UIViewController {
         }
         else {
             showDialog(message: "You have not connected to internet. Using local data.")
+        }
+    }
+    
+    func loadData() {
+        if NetworkChecker.isConnectedToInternet {
+            ProgressHUD.show("Loading users...")
+            fb.loadVector { [self] (result) in
+                kMeanVectors = result
+                print("Numver of k-Means vectors: \(kMeanVectors.count)")
+                vectorsLabel.text = "You have \(kMeanVectors.count / NUMBER_OF_K) users."
+                ProgressHUD.dismiss()
+                try! realm.write {
+                  realm.deleteAll()
+                }
+                for vector in kMeanVectors {
+                    vectorHelper.saveVector(vector)
+                }
+            }
+            fb.loadLogTimes { (result) in
+                attendList = result
+                for user in attendList {
+                    let u = User(name: user.name, image: UIImage(named: "LaunchImage")!, time: user.time)
+                    localUserList.append(u)
+                }
+            }
+        }
+        else {
+//            code for local data
+            let result = realm.objects(SavedVector.self)
+            print(result.count)
+            kMeanVectors = []
+            for vector in result {
+                let v = Vector(name: vector.name, vector: stringToArray(string: vector.vector), distance: vector.distance)
+                kMeanVectors.append(v)
+            }
+            vectorsLabel.text = "You have \(kMeanVectors.count / NUMBER_OF_K) users."
         }
     }
     
